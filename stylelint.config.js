3ff34c892027ee7ee3c007ef260473dab4a7060c
Eric Dunsworth <1907279+EricDunsworth@users.noreply.github.com>
2026-05-21 10:01:20 -0400

    WIP

---
+++ stylelint.config.js
@@ -0,0 +1,130 @@
+/** @type {import('stylelint').Config} */
+module.exports = {
+
+	plugins: [ "@double-great/stylelint-a11y" ],
+	rules: {
+
+		"alpha-value-notation": "number",
+		"color-function-notation": "legacy",
+
+		// Suppress stylelint-config-recommended errors
+		"declaration-property-value-keyword-no-deprecated": [
+			true,
+			{
+				ignoreKeywords: [
+					"break-word" // word-break: break-word; is depreciated but used
+				]
+			}
+		],
+		"font-family-no-duplicate-names": [
+			true,
+			{
+				ignoreFontFamilyNames: [
+					"monospace" // Issue from Bootstrap-sass/normalize.scss
+				]
+			}
+		],
+		"font-family-no-missing-generic-family-keyword": [
+			true,
+			{
+				ignoreFontFamilies: [
+					"Glyphicons Halflings", // Icons don't need fallbacks
+					"/a/" // Weird buggy thing from text-hide mixin from Bootstrap 3
+				]
+			}
+		],
+
+		"media-feature-range-notation": "prefix", // Uses legacy syntax
+
+		// Partially-enable stylelint-a11y rules
+		"a11y/content-property-no-static-value": null,
+		"a11y/font-size-is-readable": null,
+		"a11y/line-height-is-vertical-rhythmed": null,
+		"a11y/media-prefers-reduced-motion": null,
+		"a11y/media-prefers-color-scheme": null,
+		"a11y/no-display-none": null,
+		"a11y/no-obsolete-attribute": true,
+		"a11y/no-obsolete-element": null, // Treats menu and hgroup as false positives
+		"a11y/no-spread-text": true,
+		"a11y/no-outline-none": null,
+		"a11y/no-text-align-justify": null, // Bootstrap 3.4.x comes with a .text-justify class
+		"a11y/selector-pseudo-class-focus": null
+	},
+	overrides: [
+		{
+			files: [
+				"**/*.css"
+			],
+			extends: [
+				"stylelint-config-standard"
+			],
+			plugins: [
+				"stylelint-order"
+			],
+			rules: {
+				"function-no-unknown": null,
+				"media-feature-name-no-unknown": null,
+				"no-descending-specificity": null, // Extremely slow
+				"no-duplicate-selectors": null,
+				"no-irregular-whitespace": null,
+
+				// Suppress stylelint-config-standard errors
+				"at-rule-empty-line-before": null,
+				"at-rule-no-vendor-prefix": null,
+				"color-hex-length": null,
+				"color-function-alias-notation": null,
+				"comment-empty-line-before": null,
+				"comment-whitespace-inside": null,
+				"declaration-block-no-redundant-longhand-properties": null,
+				"declaration-block-no-duplicate-properties": null, // Needed to fix build because of bootstrap-sass/assets/stylesheets/bootstrap/normalize line 113
+				"declaration-property-value-no-unknown": null, // Uses legacy syntax
+				"declaration-property-value-keyword-no-deprecated": null, // Fix use of break-word
+				"function-name-case": null,
+				"function-url-quotes": null,
+				"hue-degree-notation": null, // Sass doesn't pad this out from 0 to 0deg
+				"length-zero-no-unit": null,
+				"media-feature-name-no-vendor-prefix": null,
+				"number-max-precision": null,
+				"property-no-deprecated": null, // Too noisy with Bootstrap 3
+				"property-no-vendor-prefix": null,
+				"rule-empty-line-before": null,
+				"selector-attribute-quotes": null,
+				"selector-class-pattern": null,
+				"selector-id-pattern": null,
+				"selector-not-notation": null,
+				"selector-no-vendor-prefix": null,
+				"selector-pseudo-element-colon-notation": null,
+				"shorthand-property-no-redundant-values": null,
+				"value-no-vendor-prefix": null,
+
+				// RIPPED FROM WET's SCSS settings... with SCSS-specific properties commented-out
+
+				// Additional rules for replacing sass-lint
+				"order/properties-alphabetical-order": true,
+
+				// Suppress stylelint-config-standard errors
+				"declaration-block-no-redundant-longhand-properties": null,
+				"no-descending-specificity": null, // Extremely slow
+				"no-invalid-position-at-import-rule": null, // Fixable, need to run sass migrator
+				// "scss/comment-no-empty": null,
+				// "scss/dollar-variable-pattern": null, // Fixable
+				// "scss/no-global-function-names": null, // Fixable, need to run sass migrator
+				"selector-class-pattern": null,
+				"selector-id-pattern": null,
+				"selector-not-notation": null,
+				"selector-pseudo-element-colon-notation": null
+
+				/*
+				TODOS:
+				-What is GCWeb doing for this vs WET?
+				-How much of these null overrides are actually relevant to life events?
+				-Are many of these useless baggage from WET/GCWeb's stone-age plugins or dependencies? Can I get rid of many of them?
+				-Add more devDependencies
+				-Verify devDependencies versions vs WET
+				-Fix linting failures
+				-Move into separate branch after my other pending PRs get merged (testing in a PR branch atm so that ESLint can ding any flaws in my stylelint config file)
+				*/
+			}
+		}
+	]
+};
