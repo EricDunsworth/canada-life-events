947b91aaa6ae341ace97eb738c521cbe814160c8
Eric Dunsworth <1907279+EricDunsworth@users.noreply.github.com>
2026-05-29 16:31:17 -0400

    WIP5 (options cleanup, better override comments)

--- stylelint.config.js
+++ stylelint.config.js
@@ -11,37 +11,15 @@ module.exports = {
 	],
 	rules: {
 
-		// "alpha-value-notation": "number", //REVIEWED
-		// "color-function-notation": "legacy", //REVIEWED
-
 		// Suppress stylelint-config-recommended errors
-
-		"media-feature-range-notation": "prefix", // Uses legacy syntax
-
-		// TODO: Can I enable some of these? Are new options available? They might all be disabled by default...
-		// Partially-enable stylelint-a11y rules
-		// "a11y/content-property-no-static-value": null,
-		// "a11y/font-size-is-readable": null,
-		// "a11y/line-height-is-vertical-rhythmed": null,
-		// "a11y/media-prefers-reduced-motion": null,
-		// "a11y/media-prefers-color-scheme": null,
-		// "a11y/no-display-none": null,
-		// "a11y/no-obsolete-attribute": true,
-		// "a11y/no-obsolete-element": null, // Treats menu and hgroup as false positives
-		// "a11y/no-spread-text": true,
-		// "a11y/no-outline-none": null,
-		// "a11y/no-text-align-justify": null, // Bootstrap 3.4.x comes with a .text-justify class
-		// "a11y/selector-pseudo-class-focus": null
-
-		// Suppress stylelint-config-recommended errors
-		"no-duplicate-selectors": null,
-		//"no-descending-specificity": null, // Extremely slow //partially-REVIEWED
+		"no-duplicate-selectors": null, // Workaround for WET/GCWeb bugs, remove once it's fixed upstream
 
 		// Suppress stylelint-config-standard errors
-		"comment-empty-line-before": null, //REVIEWED... might be too aggressive
-		"number-max-precision": null, //REVIEWED... can't undo it since what's failing is a redeclaration of a Bootstrap grid's properties... ugh
+		"comment-empty-line-before": null, // Overly aggressive, deliberately using adjacent comment blocks for line-like comments and unused selectors
+		"media-feature-range-notation": "prefix", // WET's media query notation
+		"number-max-precision": null, // Fails on a re-declaration of a Bootstrap grid's properties
 
-		// Additional rules for replacing sass-lint
-		"order/properties-alphabetical-order": true // keep this one enabled
+		// Additional rule from stylelint-order plugin
+		"order/properties-alphabetical-order": true
 	}
 };
